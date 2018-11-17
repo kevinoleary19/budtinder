456214298a7fe6682c72c8a572daae90b256b7b0
Kevin O'Leary <kevin@yeti.co>
2018-11-17 15:48:00 -0800

    two onboarding screens

--- ui/src/App.js
+++ ui/src/App.js
@@ -10,6 +10,8 @@ import client from './apollo';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import ChooseProductPage from './pages/ChooseProduct/ChooseProduct';
+import Mood from './pages/Mood/Mood';
+import WeedType from './pages/WeedType/WeedType';
 
 class App extends React.Component {
   render() {
@@ -22,6 +24,8 @@ class App extends React.Component {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/choose_product" component={ChooseProductPage} />
+                <Route exact path="/mood" component={Mood} />
+                <Route exact path="/weed_type" component={WeedType} />
               </Switch>
             </BrowserRouter>
           </div>
